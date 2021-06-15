baadcbfa79def9df3d9cbebc904dc70ba0ec9f6b
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-06-15 20:13:56 +0300

    Update ConnectDatabase
    Database kaydetme işlemi.

--- express-mongo-intro/helpers/connectDatabase.js
+++ express-mongo-intro/helpers/connectDatabase.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+//customer modeli veri tabanında oluşturulmak üzere dahil edildi.
+const Customer = require("../models/customer");
+
+// let Customer1 = new Customer({
+//   name: "Aleyna Dikal",
+//   city: "Tokat",
+// });
+
 const connectDatabase = () => {
   //mongo db ye bağlanma işlemi connect fonksiyonuna ikinci paremetre olarak verilen nesne ise Deprecation Warnings olarak adlandırılan uyarı mesajlaraını engellemek içindir.
   mongoose
@@ -12,5 +20,10 @@ const connectDatabase = () => {
     .then(() => console.log("Veri Tabani Baglantisi Basarili..."))
     .catch((err) => console.log(err));
 };
+
+//Customer Veri Tabanına Kaydetme İşlemi
+// Customer1.save()
+//   .then(() => console.log("Kaydetme Başarılı"))
+//   .catch((err) => console.log(err));
 
 module.exports = connectDatabase;
