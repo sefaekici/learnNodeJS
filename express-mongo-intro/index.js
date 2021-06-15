d838a4de1c26243ba2ea8ceb33bc46823b482d7b
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-06-15 20:14:34 +0300

    Update File Name

--- express-mongo-intro/index.js
+++ express-mongo-intro/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDatabase = require("./helpers/connectDatabase");
+const connectDatabase = require("./helpers/databaseHelper");
 
 connectDatabase();
 
