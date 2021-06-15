a354dca41367fc904113ba5b8329c1581390942b
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-06-15 21:55:26 +0300

    Update databaseHelper.js
    Veri tabanından bir kaydı güncelleme ve silme işlemleri yapıldı.

--- express-mongo-intro/helpers/databaseHelper.js
+++ express-mongo-intro/helpers/databaseHelper.js
@@ -47,52 +47,101 @@ const connectDatabase = () => {
 //Queryler wheere komutu hangi fieldın kontrol edileceğini equals ise neye eşit olacağını belirler uyuşan değerler getirilir.
 //Limit fonksiyonu ise verdiğimiz değere göre örneğin 1 verilirse ilk bulduğu kaydı 2 denilirse ilk ikisini getirir.
 //Genelde limit fonksiyonu sayfalama işlemleri için kullanılır örneğin her sayfanın 10 arlı olması gibi.
-Customer.find({}, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else console.log(data);
-})
-  .where("city")
-  .equals("Samsun")
-  .limit(1);
+// Customer.find({}, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else console.log(data);
+// })
+//   .where("city")
+//   .equals("Samsun")
+//   .limit(1);
 
-//Arabaların hepsini çek içerisinde yaşı 12 den büyük olanlardan ilkini getir. gt=>büyükleri gte=>büyük eşitleri lt=>küçükleri lte=>küçük eşitleri
-Car.find({}, (err, data) => {
+// //Arabaların hepsini çek içerisinde yaşı 12 den büyük olanlardan ilkini getir. gt=>büyükleri gte=>büyük eşitleri lt=>küçükleri lte=>küçük eşitleri
+// Car.find({}, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// })
+//   .where("age")
+//   .gt(12)
+//   .limit(1);
+
+// //12 den büyük 15 den küçük eşit olanlar
+// Car.find({}, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// })
+//   .where("age")
+//   .gt(12)
+//   .lte(15)
+//   .limit(1);
+
+// //12 den büyük 15 den küçük eşit olanların sadece model alanını getir.Select spesific bir alan getirilmek istendiğinde kullanılır.
+// Car.find({}, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// })
+//   .where("age")
+//   .gt(12)
+//   .lte(15)
+//   .limit(1)
+//   .select("model"); //("model age") model ve age i getir anlamına gelir.
+
+// //Sort
+// Car.find({}, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// })
+//   .where("age")
+//   .gt(11)
+//   .lte(15)
+//   .limit(2)
+//   .sort("name") //isime göre sıralar
+//   .select("model");
+
+//Veritabanında Güncelleme İşlemi
+
+//Veritabanında kullanıcı id si ile bulunmuştur ve o idli kullanıcı güncellenmiştir.
+Customer.findById("60c8e381a0be1b113c014725", (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    //Gelen datanın boş olup olmadığı kontrol edilir.
+    if (data != null) {
+      //bulunan customerin city değeri değiştirildi ve save fonksiyonu yardımı ile yapılan değişiklik veritabanına etki etti.
+      data.city = "Gaziantep";
+      data
+        .save()
+        .then(() => console.log("Customer is updated!"))
+        .catch((err) => console.log(err));
+    } else console.log("Data is null");
   }
-})
-  .where("age")
-  .gt(12)
-  .limit(1);
+});
 
-//12 den büyük 15 den küçük eşit olanlar
-Car.find({}, (err, data) => {
+//Veritabanından Silme İşlemi
+Customer.findById("60c8e381a0be1b113c014725", (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    if (data != null) {
+      //find by id ile bulunan kullanıcı alınan data yardımı ile veri tabanından silinebilir.
+      data
+        .remove()
+        .then(() => console.log("Customer is deleted..."))
+        .catch((err) => console.log(err));
+    } else console.log("Data is null");
   }
-})
-  .where("age")
-  .gt(12)
-  .lte(15)
-  .limit(1);
-
-//12 den büyük 15 den küçük eşit olanların sadece model alanını getir.Select spesific bir alan getirilmek istendiğinde kullanılır.
-Car.find({}, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-})
-  .where("age")
-  .gt(12)
-  .lte(15)
-  .limit(1)
-  .select("model");
+});
 
 module.exports = connectDatabase;
